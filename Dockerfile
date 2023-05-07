2d14bfcc81fb4596f41d3e097370a45d5059a40d
nara-ryoya <nara_ryoya@icloud.com>
2023-05-07 22:31:08 +0900

    chore

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM nvidia/cuda:11.7.0-devel-ubuntu22.04
+FROM nvidia/cuda:11.7.0-devel-ubuntu20.04
 
 ENV DEBIAN_FRONTEND=noninteractive
 ENV PIP_DEFAULT_TIMEOUT=100
@@ -7,18 +7,31 @@ RUN apt-get update && apt-get upgrade -y
 RUN apt-key adv --keyserver keyserver.ubuntu.com --recv-keys F39F6D246A59DDE01C866C425A78330DFF0A946E
 RUN apt-get update && \
     apt-get upgrade && \
-    apt-get -y install locales git vim python3.10 build-essential libffi-dev libssl-dev zlib1g-dev liblzma-dev libbz2-dev libreadline-dev libsqlite3-dev libopencv-dev tk-dev git pip && \
-    pip install poetry && \
-    ln -s $(which python3) /usr/bin/python
+    apt-get -y install locales git vim curl build-essential libffi-dev libssl-dev zlib1g-dev liblzma-dev libbz2-dev libreadline-dev libsqlite3-dev libopencv-dev tk-dev git pip
 
 WORKDIR /root/workdir/
+
+# pyenv
+RUN git clone https://github.com/pyenv/pyenv.git ~/.pyenv && \
+    echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc && \
+    echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc && \
+    echo 'eval "$(pyenv init --path)"' >> ~/.bashrc
+RUN  bash -c 'source ~/.bashrc;'
+
+RUN /root/.pyenv/bin/pyenv install 3.10.6 && \
+    /root/.pyenv/bin/pyenv local 3.10.6
+
+RUN /root/.pyenv/shims/pip install --upgrade pip && \
+    /root/.pyenv/shims/pip install poetry
 
 COPY pyproject.toml ./
 
 COPY .torch_wheels ./.torch_wheels
 
-# RUN poetry config virtualenvs.create false && \
-#     poetry install
+RUN ls /root/.pyenv/shims
+
+RUN  /root/.pyenv/shims/poetry config virtualenvs.create false && \
+    /root/.pyenv/shims/poetry install
 
 
 CMD ["/bin/bash"]
--- train.py
+++ train.py
@@ -3,12 +3,17 @@ from torchvision.datasets import MNIST
 from torch.utils.data import DataLoader
 from torchvision import transforms
 from torch import optim, nn
+import torch
 from tqdm import tqdm
+
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 
 model = resnet50(pretrained=True)
 num_ftrs = model.fc.in_features
 model.fc = nn.Linear(num_ftrs, 10)
+
+model = model.to(device)
 
 transform = transforms.Compose([
     transforms.Resize(224),
@@ -19,7 +24,7 @@ transform = transforms.Compose([
 
 train_dataset, validation_dataset = MNIST(root='./data', download=True, train=True, transform=transform), MNIST(root='./data', download=True, train=False, transform=transform)
 
-train_dataloader, test_dataloader = DataLoader(train_dataset, batch_size=32, shuffle=True), DataLoader(validation_dataset, batch_size=32, shuffle=False)
+train_dataloader, test_dataloader = DataLoader(train_dataset, batch_size=128, shuffle=True, num_workers=2, pin_memory=True), DataLoader(validation_dataset, batch_size=128, shuffle=False, num_workers=2, pin_memory=True)
 
 
 criterion = nn.CrossEntropyLoss()
@@ -28,8 +33,11 @@ optimizer = optim.Adam(model.parameters(), lr=0.001)
 epoch_num = 10
 
 for epoch in tqdm(range(epoch_num)):
+    print(f'Epoch {epoch + 1}')
     running_loss = 0.0
-    for i, (inputs, labels) in tqdm(enumerate(train_dataloader)):
+    model.train()
+    for i, (inputs, labels) in enumerate(train_dataloader):
+        inputs, labels = inputs.to(device), labels.to(device)
         optimizer.zero_grad()
 
         outputs = model(inputs)
@@ -38,6 +46,21 @@ for epoch in tqdm(range(epoch_num)):
         optimizer.step()
 
         running_loss += loss.item()
-        if i % 100 == 99:
-            print(f'[{epoch + 1}, {i + 1}] loss: {running_loss / 100}')
+        if i % 10 == 9:
+            print(f'[{epoch + 1}, {i + 1} / {len(train_dataloader)}] loss: {running_loss / 100}')
             running_loss = 0.0
+
+    with torch.no_grad():
+        model.eval()
+        all_correct_sum = 0
+        all_sample_num = 0
+        for i, (inputs, labels) in tqdm(enumerate(test_dataloader)):
+            inputs, labels = inputs.to(device), labels.to(device)
+
+            outputs = model(inputs)
+            correct_sum = torch.sum(torch.argmax(outputs, dim=1) == labels)
+            all_correct_sum += correct_sum
+            all_sample_num += len(labels)
+        print(f'Accuracy: {all_correct_sum / all_sample_num}')
+
+    torch.save(model.state_dict(), f'./models/model_{epoch + 1}_acc_{(all_correct_sum / all_sample_num):.2}.pth')
